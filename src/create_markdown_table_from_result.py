6ae05bc18cd9f383c7d9d4c8d05ecca7fcc12b39
Matei-Marius Micu <matei.micu@mambu.com>
2019-11-01 16:57:49 +0200

    Add tools to generate markdown tables

---
+++ src/create_markdown_table_from_result.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+"""
+Create a markdown Table from a summary file.
+"""
+import argparse
+import os
+import json
+
+
+def get_parser():
+    """Return a CLI parser."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('SOURCE', type=str,
+                        help='JSON summary')
+
+    parser.add_argument('DEST', type=str,
+                        help='Destination of the markdown file')
+
+    return parser
+
+
+def main():
+    """Main entry point."""
+    parser = get_parser()
+    args = parser.parse_args()
+
+    data = json.loads(open(os.path.abspath(args.SOURCE), 'r').read())
+
+    with open(args.DEST, 'w+') as fd:
+        fd.writelines([
+            '# Tabular data of the experiment \n',
+            '\n',
+            '\n',
+            '| Model Name | Nr. Orders | Nr. Items  |   Profit   | Duration (seconds) |\n',
+            '|------------|------------|------------|------------|--------------------|\n',
+        ])
+        for item in data['models']:
+            # import pdb; pdb.set_trace()
+            # try:
+            time = float(item['delta_time'])
+            # if time / 60 > 30:
+            #     time = time / 60.0
+
+            fd.write(
+                '| {:10.10} | {:10.10} | {:10.10} | {:10.10} | {:18.2f} |\n'.format(
+                    item['name'],
+                    str(item['nr_orders']),
+                    str(item['nr_items']),
+                    float(item['profit']),
+                    time,
+                    ) 
+            )
+            # except Exception as exc:
+                # import pdb; pdb.set_trace()
+
+    print('Done ...')
+if __name__ == '__main__':
+    main()
